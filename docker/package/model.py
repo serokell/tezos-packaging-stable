2ca684fbfd31406e5321135edde01a23ead43200
Roman Melnikov <roman.melnikov@serokell.io>
2022-01-14 13:01:37 +0300

    [#349] Add way to reset custom baking services
    Problem: One may need to reset a node that runs custom network. E.g.
    it's used in some specific short scenario that incorporates small
    blockchain, or it runs a blockchain that resets on daily basis.
    
    Solution: Add oneshot service that will stop the corresponding instance
    of the custom baking service and clean up the custom node data
    directory.

--- docker/package/model.py
+++ docker/package/model.py
@@ -558,6 +558,27 @@ class TezosBakingServicesPackage(AbstractPackage):
                 "custom",
             )
         )
+        self.systemd_units.append(
+            SystemdUnit(
+                service_file=ServiceFile(
+                    Unit(
+                        after=[],
+                        description=f"One-shot service to reset custom baking service",
+                    ),
+                    Service(
+                        exec_start="/usr/bin/tezos-baking-custom-reset %i",
+                        user="root",
+                        state_directory="tezos",
+                        remain_after_exit=False,
+                        type_="oneshot",
+                    ),
+                    Install(wanted_by=["multi-user.target"]),
+                ),
+                instances=[],
+                suffix="custom-reset",
+                startup_script="tezos-baking-custom-reset",
+            )
+        )
         self.postinst_steps = ""
         self.postrm_steps = ""
 
