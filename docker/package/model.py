2ca92b0de095999016b5cc1fb9edd26e69080f4e
pasqu4le <pasqu4le@gmail.com>
2022-10-28 00:55:46 +0200

    [#517] Use node env file in baking service
    Problem: we now have a node env files with the
    options that the define how the node works
    without duplication.
    However, some of these options need to be
    known by baking services too.
    
    Solution: add the node environment file to the
    baking instances definitions.
    Additionally, remove CUSTOM_NODE_CONFIG from baking
    config too.

--- docker/package/model.py
+++ docker/package/model.py
@@ -576,7 +576,10 @@ class TezosBakingServicesPackage(AbstractPackage):
                 self.__gen_baking_systemd_unit(
                     requires,
                     f"Tezos baking instance for {network}",
-                    [f"/etc/default/tezos-baking-{network}"],
+                    [
+                        f"/etc/default/tezos-baking-{network}",
+                        f"/etc/default/tezos-node-{network}",
+                    ],
                     "tezos-baking.conf",
                     network,
                 )
@@ -588,7 +591,10 @@ class TezosBakingServicesPackage(AbstractPackage):
         custom_unit = self.__gen_baking_systemd_unit(
             custom_requires,
             f"Tezos baking instance for custom network",
-            [f"/etc/default/tezos-baking-custom@%i"],
+            [
+                f"/etc/default/tezos-baking-custom@%i",
+                f"/etc/default/tezos-node-custom@%i",
+            ],
             "tezos-baking-custom.conf",
             "custom",
         )
