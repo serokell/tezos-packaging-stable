088ecda7d17470ba8f80ff5511b87fac6bb5b160
Savely Krendelhoff <alicecandyom@proton.me>
2023-12-27 18:36:19 +0300

    [Chore] Bump Tezos sources to v19.0-rc1

--- scripts/update-tezos.sh
+++ scripts/update-tezos.sh
@@ -43,25 +43,25 @@ if [[ "$latest_upstream_tag" = "$our_tezos_tag" ]]; then
     echo "buber" >> ./docker/octez_executables
     git commit -a -m "[Chore] Bump Tezos sources to $latest_upstream_tag"
 
-    ./scripts/update-brew-formulae.sh "$latest_upstream_tag-1"
-    git commit -a -m "[Chore] Update brew formulae for $latest_upstream_tag"
+    #./scripts/update-brew-formulae.sh "$latest_upstream_tag-1"
+    #git commit -a -m "[Chore] Update brew formulae for $latest_upstream_tag"
 
-    sed -i 's/"release": "[0-9]\+"/"release": "1"/' ./meta.json
-    # Update version of tezos-baking package
-    sed -i "s/version = .*/version = \"$latest_upstream_tag\"/" ./baking/pyproject.toml
-    # Commit may fail when release number wasn't updated since the last release
-    git commit -a -m "[Chore] Reset release number for $latest_upstream_tag" || \
-      echo "release number wasn't updated"
+    #sed -i 's/"release": "[0-9]\+"/"release": "1"/' ./meta.json
+    ## Update version of tezos-baking package
+    #sed -i "s/version = .*/version = \"$latest_upstream_tag\"/" ./baking/pyproject.toml
+    ## Commit may fail when release number wasn't updated since the last release
+    #git commit -a -m "[Chore] Reset release number for $latest_upstream_tag" || \
+    #  echo "release number wasn't updated"
 
-    sed -i 's/letter_version *= *"[a-z]"/letter_version = ""/' ./docker/package/model.py
-    # Commit may fail when the letter version wasn't updated since the last release
-    git commit -a -m "[Chore] Reset letter_version for $latest_upstream_tag" || \
-      echo "letter_version wasn't reset"
+    #sed -i 's/letter_version *= *"[a-z]"/letter_version = ""/' ./docker/package/model.py
+    ## Commit may fail when the letter version wasn't updated since the last release
+    #git commit -a -m "[Chore] Reset letter_version for $latest_upstream_tag" || \
+    #  echo "letter_version wasn't reset"
 
-    ./scripts/update-release-binaries.py
-    git commit -a -m "[Chore] Update release binaries for $latest_upstream_tag"
+    #./scripts/update-release-binaries.py
+    #git commit -a -m "[Chore] Update release binaries for $latest_upstream_tag"
 
-    #git push --set-upstream origin "$branch_name"
+    ##git push --set-upstream origin "$branch_name"
 
     #gh pr create -B master -t "[Chore] $latest_upstream_tag release" -F .github/release_pull_request_template.md
   fi
